b094bc4e8aa70df881a2fdea1d5b922e3836813b
saraxplorer <147872964+saraxplorer@users.noreply.github.com>
2025-01-13 15:48:33 +0100

    Update tree_height.c

--- binary tree/tree_height/tree_height.c
+++ binary tree/tree_height/tree_height.c
@@ -1,14 +1,9 @@
-typedef struct s_tree
-{
-    int value;
-    struct s_tree *left;
-    struct s_tree *right;
-} t_tree;
+#include "ft_btree.h"
 
-int tree_height(t_tree *tree)
+int tree_height(t_btree *tree)
 {
     if (!tree)
-        return (-1);//The height of an empty tree is `-1`, coz for a tree with only one node, it is `0`.
+        return (0);
     int left_height = tree_height(tree->left);
     int right_height = tree_height(tree->right);
     if (left_height > right_height)
@@ -17,5 +12,7 @@ int tree_height(t_tree *tree)
         return (right_height + 1);
 }
 
+//Although The height of an empty tree is `-1`, coz for a tree with only one node, it is `0`.
+//we return 0 here because the subject asked so.
 //return the larger one plus 1
 
